8e1b23e7db162e403da2e82e98308b2b104429cd
Giulio Starace <giulio.starace@gmail.com>
2025-03-08 23:38:59 +0100

    crossformer on raw without emb table

--- giulio/crossformer.py
+++ giulio/crossformer.py
@@ -1,5 +1,7 @@
+from einops import rearrange
 import torch
 from torch import nn
+from twostage_attention import TwoStageAttentionLayer
 
 
 class CrossFormer(nn.module):
@@ -8,12 +10,109 @@ class CrossFormer(nn.module):
         source_num_channels: int,
         target_num_channels: int,
         max_sequence_length: int,
+        num_heads: int,
+        num_enc_layers: int,
+        num_dec_layers: int,
     ) -> None:
         super().__init__()
 
         self.source_num_channels = source_num_channels
         self.target_num_channels = target_num_channels
         self.max_sequence_length = max_sequence_length
+        self.num_heads = num_heads
+        self.num_enc_layers = num_enc_layers
+        self.num_dec_layers = num_dec_layers
+
+        self.encoder = EncoderBlock(
+            self.max_sequence_length,
+            self.source_num_channels,
+            self.num_heads,
+            self.num_enc_layers,
+        )
+        self.decoder = DecoderBlock(
+            self.max_sequence_length,
+            self.target_num_channels,
+            self.num_heads,
+            self.num_dec_layers,
+        )
 
     def forward(self, src: torch.Tensor, tgt: torch.Tensor) -> torch.Tensor:
-        pass
+        encoder_output = self.encoder(src)
+
+        decoder_output = self.decoder(tgt, encoder_output)
+
+        return decoder_output
+
+    def _create_causal_mask(self, size: int) -> torch.Tensor:
+        """Create a causal mask for the decoder. Prevents decoder from attending to future tokens."""
+        mask = torch.triu(torch.ones(size, size) * float("-inf"), diagonal=1)
+        return mask.to(dtype=torch.float)
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, seq_len: int, num_channels: int, num_heads: int, n_layers: int):
+        super().__init__()
+        self.layers = nn.ModuleList(
+            [
+                TwoStageAttentionLayer(seq_len, num_channels, num_heads)
+                for _ in range(n_layers)
+            ]
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        args:
+            x: torch.tensor of shape (batch, seq_len, src_channels)
+
+        returns:
+            torch.tensor of shape (batch, seq_len, src_channels)
+        """
+        for encoder in self.self_attn_layers:
+            x = encoder(x)
+
+        return x
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, seq_len, num_channels, num_heads, n_layers):
+        super().__init__()
+        self.self_attn_layers = nn.ModuleList(
+            [
+                TwoStageAttentionLayer(seq_len, num_channels, num_heads)
+                for _ in range(n_layers)
+            ]
+        )
+        self.cross_attn_layers = nn.ModuleList(
+            [
+                nn.MultiheadAttention(
+                    embed_dim=num_channels, num_heads=num_heads, batch_first=True
+                )
+                for _ in range(n_layers)
+            ]
+        )
+
+    def forward(self, x: torch.Tensor, encoder_output: torch.Tensor) -> torch.Tensor:
+        """
+        args:
+            x: torch.tensor of shape (batch, seq_len, tgt_channels)
+            encoder_output: torch.tensor of shape (batch, seq_len, src_channels)
+
+        returns:
+            torch.tensor of shape (batch, seq_len, tgt_channels)
+        """
+        for self_attn_layer, cross_attn_layer in zip(
+            self.self_attn_layers, self.cross_attn_layers
+        ):
+            # b, s, tgt_channels
+            self_attn_output = self_attn_layer(x)
+
+            # b, s, tgt_channels
+            cross_attn_output = cross_attn_layer(
+                # b, s, tgt_channels
+                self_attn_output,
+                # b, s, src_channels
+                encoder_output,
+                encoder_output,
+            )
+
+        return cross_attn_output
--- giulio/twostage_attention.py
+++ giulio/twostage_attention.py
@@ -11,96 +11,66 @@ class TwoStageAttentionLayer(nn.Module):
     input/output shape: [batch_size, number_of_channels (D), number_of_timesegments (L), d_model]
     """
 
-    def __init__(self, ts_length, embed_dim, num_heads, ff_dim=None, dropout=0.1):
+    def __init__(self, seq_len, num_channels, num_heads, dropout=0.1):
         super().__init__()
-        ff_dim = ff_dim or 4 * embed_dim
+        time_embd_dim = num_channels
+        time_ff_dim = 4 * time_embd_dim
         self.time_attention = nn.MultiheadAttention(
-            embed_dim, num_heads, batch_first=True, dropout=dropout, bias=False
+            time_embd_dim, num_heads, batch_first=True, dropout=dropout, bias=False
+        )
+        self.norm1 = nn.LayerNorm(time_embd_dim)
+        self.norm2 = nn.LayerNorm(time_embd_dim)
+        self.MLP_time = nn.Sequential(
+            nn.Linear(time_embd_dim, time_ff_dim),
+            nn.GELU(),
+            nn.Linear(time_ff_dim, time_embd_dim),
         )
 
-        self.dimension_attention = nn.MultiheadAttention(
-            embed_dim, num_heads, batch_first=True, dropout=dropout, bias=False
+        channel_embd_dim = seq_len
+        channel_ff_dim = 4 * channel_embd_dim
+        self.channel_attention = nn.MultiheadAttention(
+            channel_embd_dim, num_heads, batch_first=True, dropout=dropout, bias=False
         )
+        self.norm3 = nn.LayerNorm(channel_embd_dim)
+        self.norm4 = nn.LayerNorm(channel_embd_dim)
+        self.MLP_channel = nn.Sequential(
+            nn.Linear(channel_embd_dim, channel_ff_dim),
+            nn.GELU(),
+            nn.Linear(channel_ff_dim, channel_embd_dim),
+        )
+
         self.dropout = nn.Dropout(dropout)
 
-        self.norm1 = nn.LayerNorm(embed_dim)
-        self.norm2 = nn.LayerNorm(embed_dim)
-        self.norm3 = nn.LayerNorm(embed_dim)
-        self.norm4 = nn.LayerNorm(embed_dim)
-        self.MLP1 = nn.Sequential(
-            nn.Linear(embed_dim, ff_dim), nn.GELU(), nn.Linear(ff_dim, embed_dim)
-        )
-        self.MLP2 = nn.Sequential(
-            nn.Linear(embed_dim, ff_dim), nn.GELU(), nn.Linear(ff_dim, embed_dim)
-        )
-
-    def forward(
-        self,
-        x: torch.Tensor,
-        attention_mask_time: Optional[torch.Tensor] = None,
-        key_padding_mask_time: Optional[torch.Tensor] = None,
-        attention_mask_dimension: Optional[torch.Tensor] = None,
-    ) -> torch.Tensor:
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Applies TSA. First, applies attention on the time dimension. Then, on the channel dimension.
         Derived from official implementation of https://openreview.net/pdf?id=vSVLM2j9eie
-        """
-        batch, timesteps, channels, embed_dim = x.shape
 
-        # Cross Time Stage: Apply MSA across time
-        time_in = rearrange(
-            x,
-            "b ts_length ts_dim d_model -> (b ts_dim) ts_length d_model",
-        )
+        x: torch.tensor of shape (batch, seq_len, num_channels)
+        """
+        batch, timesteps, num_channels = x.shape
 
         time_in = self.norm1(time_in)
-        # self-attend all tokens time wise
-        time_enc, _ = self.time_attention(
-            time_in,
-            time_in,
-            time_in,
-            attn_mask=attention_mask_time,
-            key_padding_mask=key_padding_mask_time,
-        )
+        time_enc, _ = self.time_attention(time_in, time_in, time_in)
         time_out = time_in + self.dropout(time_enc)
-        # time_out = self.norm1(time_out)
 
         time_out = self.norm2(time_out)
-        ff_out = self.MLP1(time_out)
+        ff_out = self.MLP_time(time_out)
         time_out = time_out + self.dropout(ff_out)
-        # time_out = self.norm2(time_out)
 
-        # Cross Dimension Stage: Use normal attention across dimensions instead of router
-        dimension_in = rearrange(
-            time_out,
-            "(b ts_dim) ts_length d_model -> (b ts_length) ts_dim d_model",
-            b=batch,
-            ts_dim=channels,
-            ts_length=timesteps,
-            d_model=embed_dim,
+        channel_in = rearrange(
+            time_out, "b t c -> b c t", b=batch, t=timesteps, c=num_channels
         )
+        channel_in = self.norm3(channel_in)
+        channel_enc, _ = self.channel_attention(channel_in, channel_in, channel_in)
+        channel_out = channel_in + self.dropout(channel_enc)
 
-        # Apply normal self-attention across dimensions, similar to time attention
-        dimension_in = self.norm3(dimension_in)
-        dimension_enc, _ = self.dimension_attention(
-            dimension_in,
-            dimension_in,
-            dimension_in,
-            attn_mask=attention_mask_dimension,
-        )
-
-        dimension_out = dimension_in + self.dropout(dimension_enc)
-        # dimension_out = self.norm3(dimension_out)
-
-        dimension_out = self.norm4(dimension_out)
-        ff_out = self.MLP2(dimension_out)
-        dimension_out = dimension_out + self.dropout(ff_out)
-        # dimension_out = self.norm4(dimension_out)
+        channel_out = self.norm4(channel_out)
+        ff_out = self.MLP_channel(channel_out)
+        channel_out = channel_out + self.dropout(ff_out)
 
         twostage_output = rearrange(
-            dimension_out,
-            "(b ts_length) ts_dim d_model -> b ts_length ts_dim d_model",
-            b=batch,
+            channel_out, "b c t -> b t c", b=batch, t=timesteps, c=num_channels
         )
 
         return twostage_output
